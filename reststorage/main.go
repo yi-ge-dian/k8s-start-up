5322979f08f4b557a8a9c7af100a70beebbf0994
yi-ge-dian <1085266008@qq.com>
2023-05-18 15:11:50 +0800

    feat(registry): finish registry

--- reststorage/main.go
+++ reststorage/main.go
@@ -4,51 +4,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// 创建任务的 API 处理函数
-func createTaskHandler(c *gin.Context) {
-	var task Task
-	if err := c.ShouldBindJSON(&task); err != nil {
-		c.JSON(400, gin.H{"error": "Bad Request"})
-		return
-	}
-	taskRegistry := TaskRegistry{}
-	err := taskRegistry.CreateTask(task)
-	if err != nil {
-		c.JSON(500, gin.H{"error": "Failed to create task"})
-		return
-	}
-	c.JSON(201, gin.H{"message": "Task created successfully"})
-}
-
-// 获取任务列表的API处理函数
-func listTasksHandler(c *gin.Context) {
-	taskRegistry := TaskRegistry{}
-	tasks, err := taskRegistry.ListTasks()
-	if err != nil {
-		c.JSON(500, gin.H{"error": "Failed to listtasks"})
-		return
-	}
-	c.JSON(200, tasks)
-}
-
-// 展示服务的 API 处理函数
-func listServicesHandler(c *gin.Context) {
-	c.JSON(200, nil)
-}
-
-// 创建服务的 API 处理函数
-func createServicesHandler(c *gin.Context) {
-	c.JSON(200, nil)
-}
-
 func main() {
 	// 初始化 Gin Web框架
 	r := gin.Default()
 	// 注册 API 路由
-	//r.POST("/tasks", createTaskHandler)
-	//r.POST("/services", createServicesHandler)
-	//r.GET("/tasks", listTasksHandler)
-	//r.GET("/services", listServicesHandler)
 	r.Any("resource/:type", restHandler)
 	//启动 Web 服务器
 	_ = r.Run(":8080")
@@ -66,19 +25,40 @@ type Service struct {
 	Description string
 }
 
-type TaskRegistry struct {
+type TaskRegistry interface {
+	ListTasks() ([]Task, error)
+	CreateTask(task Task) error
 }
 
-func (t *TaskRegistry) ListTasks() ([]Task, error) {
+type MockTaskRegistry struct {
+}
+
+func (t *MockTaskRegistry) ListTasks() ([]Task, error) {
 	return []Task{
 		{
-			Name:        "testTask",
-			Description: "testTask",
+			Name:        "testMockTaskRegistry",
+			Description: "testMockTaskRegistry",
 		},
 	}, nil
 }
 
-func (t *TaskRegistry) CreateTask(task Task) error {
+func (t *MockTaskRegistry) CreateTask(task Task) error {
+	return nil
+}
+
+type MysqlTaskRegistry struct {
+}
+
+func (t *MysqlTaskRegistry) ListTasks() ([]Task, error) {
+	return []Task{
+		{
+			Name:        "testMysqlTaskRegistry",
+			Description: "testMysqlTaskRegistry",
+		},
+	}, nil
+}
+
+func (t *MysqlTaskRegistry) CreateTask(task Task) error {
 	return nil
 }
 
@@ -107,7 +87,8 @@ type TaskStorage struct {
 }
 
 func (t *TaskStorage) List(c *gin.Context) {
-	taskRegistry := TaskRegistry{}
+	// 这里需要使用哪个 Registry 直接修改即可
+	taskRegistry := MysqlTaskRegistry{}
 	tasks, err := taskRegistry.ListTasks()
 	if err != nil {
 		c.JSON(500, gin.H{"error": "Failed to list tasks"})
@@ -122,7 +103,8 @@ func (t *TaskStorage) Create(c *gin.Context) {
 		c.JSON(400, gin.H{"error": "Bad Request"})
 		return
 	}
-	taskRegistry := TaskRegistry{}
+	// 这里需要使用哪个 Registry 直接修改即可
+	taskRegistry := MysqlTaskRegistry{}
 	err := taskRegistry.CreateTask(task)
 	if err != nil {
 		c.JSON(500, gin.H{"error": "Failed to createTask"})
